53e2a0f4a5c0053bf17f30eeccf2f65c09bf0dab
abderrahim AZIME <azime_1@yahoo.fr>
2016-01-09 22:19:10 +0100

    history added

---
+++ history.md
@@ -0,0 +1,13 @@
+##Change Log
+
+* 0.3.0
+    * order data by columns desc, asc: asc default value
+    * rename get_value(self, col, row) by get_value(self, row, col), <b style='color:red'>SORRY !</b>
+    
+* 0.2.0
+    * order data by columns and column type: float
+    
+* 0.1.0
+    * load data from csv file
+    * check required columns
+    * order data by columns and column type: string, integer, datetime
--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 
 from setuptools import setup, find_packages
 import pycsv
+import io
 
 setup(
     name='pycsv',
@@ -10,13 +11,13 @@ setup(
     author='Abderrahim AZIME',
     author_email='azime_1@yahoo.fr',
     description='Simple module for reading csv file',
+    long_description=io.open("history.md", encoding='utf8').read(),
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
     platforms='any',
     test_suite = 'nose.collector',
     classifiers=[
-        "Programming Language :: Python",
         "Programming Language :: Python :: 2.7"
     ]
 )
